8914dfa3923586500bb1310d2706841c7e908304
farida-O <faridaomar150@gmail.com>
2023-03-16 22:40:17 +0200

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /* Description: printing using puts.*/
-int main(void)
+int main(void)/*start the program*/
 {
 /*print*/
 puts("\"Programming is like building a multilingual puzzle");
