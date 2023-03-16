250d1db8b208827ad9a542e93f045b2a95ca05f8
farida-O <faridaomar150@gmail.com>
2023-03-16 22:46:35 +0200

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints using printf
+ * Return: Always 0
+ */
+int main(void)
+{
+printf("\"Programming is like building a multilingual puzzle");
+return (0);
+}
