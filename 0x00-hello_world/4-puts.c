2eae8401f374938ff422b8c7214e690fcc6d2419
farida-O <faridaomar150@gmail.com>
2023-03-16 22:38:15 +0200

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,10 @@
 /* Description: printing using puts.*/
 int main(void)
 {
+/**
+	 * Description:  A column of asterisks on the left side,
+	 * with beginning and ending almost-blank lines.
+	 */
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
