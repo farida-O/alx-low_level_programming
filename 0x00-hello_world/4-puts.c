77f9258b48f8d4e0ed2dccf8c5f8097eb8c43d1e
farida-O <faridaomar150@gmail.com>
2023-03-16 22:44:05 +0200

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * 
  * Description: prints using puts
  * @parameter: describe the parameter
- * 
  * Return: Always 0 (Success)
  */
 int main(void)
