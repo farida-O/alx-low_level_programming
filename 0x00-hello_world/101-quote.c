11d24f6137cc085bf877916fa5a47e7b233e1e60
farida-O <faridaomar150@gmail.com>
2023-03-16 23:17:39 +0200

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 /**
  * main - Entry point
  * Description: prints using printf
@@ -7,8 +7,8 @@
  */
 int main(void)
 {
-const char * message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-int len = strlen(*message);
+string message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+int len = strlen(message);
 write(STDOUT_FILENO, message, len);
 return (1);
 }
