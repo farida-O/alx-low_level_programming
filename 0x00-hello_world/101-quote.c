5b9266164ff9fe6c6239e689858fc0f96a59eab7
farida-O <faridaomar150@gmail.com>
2023-03-16 23:15:10 +0200

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-char message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+const char * message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 int len = strlen(message);
 write(STDOUT_FILENO, message, len);
 return (1);
