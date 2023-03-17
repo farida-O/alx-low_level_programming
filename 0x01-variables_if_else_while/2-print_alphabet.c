94d424530135dc621a3b69ad7d73fa14a80f08ca
farida-O <faridaomar150@gmail.com>
2023-03-18 01:25:07 +0200

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Description: check if the number if positive or negative
+ * Return: Always 0
+ */
+int main(void)
+{
+char c = 'a';
+while(c <= 'z')
+{
+putchar(c);
+c++;
+}
+putchar("\n");
+return (0);
+}
