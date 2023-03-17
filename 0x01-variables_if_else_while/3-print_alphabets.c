4bef2f6a2757b77d14ccabc6df1e45632fc239c6
farida-O <faridaomar150@gmail.com>
2023-03-18 01:29:51 +0200

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Description: print alphabets
+ * Return: Always 0
+ */
+int main(void)
+{
+char c = 'a';
+while (c <= 'Z')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
