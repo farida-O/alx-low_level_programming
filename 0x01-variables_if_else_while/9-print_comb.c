2787821d5dd382cde22127501c697489bf3122aa
farida-O <faridaomar150@gmail.com>
2023-03-18 03:17:43 +0200

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: print numbers
+ * Return: Always 0
+ */
+int main(void)
+{
+int i = '0';
+for (; i <= '9' ; i++)
+{
+putchar(i);
+if ( i < 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+return (0);
+}
