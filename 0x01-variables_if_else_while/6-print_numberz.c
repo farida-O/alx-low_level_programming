cc21207ea3fdd13c1849eecb204271d24b190898
farida-O <faridaomar150@gmail.com>
2023-03-18 02:53:41 +0200

    5

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: print numbers
+ * Return: Always 0
+ */
+int main(void)
+{
+int i = '0';
+for (; i <= '9' ; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
