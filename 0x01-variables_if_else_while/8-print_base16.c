26b794eacb1a20729b5f1ca8d11a20032b5bcd47
farida-O <faridaomar150@gmail.com>
2023-03-20 06:31:25 +0200

    8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: print alphabets
+ * Description: print Hex
  * Return: Always 0
  */
 int main(void)
 {
-char c = '0';
+int c = '0';
 while (c <= 'f')
 {
 putchar(c);
 if (c == '9')
 {
-c += 87;
+c += 39;
 }
 c++;
 }
