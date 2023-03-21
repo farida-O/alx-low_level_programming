fe91bc3d2d30fda1297d8c0646c6ab344a1d03e9
farida-O <faridaomar150@gmail.com>
2023-03-21 18:02:37 +0200

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,16 +2,17 @@
 #include "main.h"
 /**
  * main - Entry point
- * Description: print string
+ * Description: print '_putchar'
  * Return: Always 0
  */
 int main(void)
 {
 char c[] = "_putchar";
-int i =0;
-for (;i < (int)strlen(c);i++)
+int i = 0;
+for (; i < (int)strlen(c); i++)
 {
 _putchar(c[i]);
 }
+_putchar('/n');
 return (0);
 }
