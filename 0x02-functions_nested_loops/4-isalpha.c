d1517832c30000a31ba90530f8c6240b9b891a03
farida-O <faridaomar150@gmail.com>
2023-03-22 00:51:30 +0200

    4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include <ctype.h>
 #include "main.h"
 /**
- * _islower - Determine whether it is an ahphabetic character
+ * _isalpha - Determine whether it is an ahphabetic character
  * Description: using islower
  * @c: input Character
  * Return: 0 in case it's an ahphabetic character and 1 otherwise
