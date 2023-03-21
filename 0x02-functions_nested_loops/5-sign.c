6f7988239dfe0e254393ec30f56b24ccdd5c4c0e
farida-O <faridaomar150@gmail.com>
2023-03-22 00:59:24 +0200

    5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,7 +4,8 @@
  * print_sign - Determine the number polarity
  * Description: print 0, 1, or -1 based on the sign of the number
  * @n: input number
- * Return: 1 in case it's a positive number, 0 if it equals to zero, and -1 if it's negative
+ * Return: 1 in case it's a positive number,
+ * 0 if it equals to zero, and -1 if it's negative
  */
 int print_sign(int n)
 {
