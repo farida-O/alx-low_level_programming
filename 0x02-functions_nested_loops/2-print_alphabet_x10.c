ebd2326c32b274bff4939275065c001adc4a3851
farida-O <faridaomar150@gmail.com>
2023-03-21 18:21:29 +0200

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * print_alphabet - function
- * Description: print alphabets
+ * print_alphabet_x10 - print alphabets
+ * Description: using _putchar
  * return: void
  */
 void print_alphabet_x10(void)
