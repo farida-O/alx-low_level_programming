78fc32bcbbe9bad2b0756cf6c734a728379541a5
farida-O <faridaomar150@gmail.com>
2023-03-22 00:58:42 +0200

    5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,7 +10,7 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-_putchar('1');
+_putchar('+');
 return (1);
 }
 else if (n == 0)
@@ -20,7 +20,7 @@ return (0);
 }
 else
 {
-_putchar(-1);
+_putchar('-');
 return (-1);
 }
 }
