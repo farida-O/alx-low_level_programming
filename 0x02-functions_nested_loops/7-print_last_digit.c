fe24e00e91831e3a9b07a71150ba8b5873e67e8d
farida-O <faridaomar150@gmail.com>
2023-03-22 01:29:01 +0200

    7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,7 +10,6 @@ int print_last_digit(int n)
 {
 int last_digit;
 last_digit = n - (int)(n / 10) * 10;
-last_digit = abs(last_digit);
-_putchar((char)last_digit);
+_putchar(last_digit);
 return (last_digit);
 }
