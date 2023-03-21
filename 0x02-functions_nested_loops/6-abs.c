65658267bac9af0327d277c797197e2f67a55957
farida-O <faridaomar150@gmail.com>
2023-03-22 01:12:38 +0200

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _abs - Determine absolute of a value
+ * Description: 
+ * @n : input value
+ * Return: abs value
+ */
+int _abs(int n)
+{
+return (abs(n));
+}
