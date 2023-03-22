58fad629e00b51aac81b2bedb7622bc70bb437c9
farida-O <faridaomar150@gmail.com>
2023-03-22 02:55:21 +0200

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,7 +10,7 @@ void times_table(void)
 int i4 = 0;
 int i3 = 0;
 int n;
-for (; i4 <= 0; i4++)
+for (; i4 <= 9; i4++)
 {
 for (; i3 <= 9; i3++)
 {
@@ -19,6 +19,7 @@ _putchar('0'+ n);
 if (i3 != 9)
 {
 _putchar(',');
+_putchar('\t');
 }
 }
 _putchar('\n');
