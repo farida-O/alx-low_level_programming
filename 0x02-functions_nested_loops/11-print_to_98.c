b8053b327dedb74415fa6c7eaedf193ea0abc144
farida-O <faridaomar150@gmail.com>
2023-03-22 04:52:03 +0200

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,7 @@ void print_to_98(int n)
 {
 int plus = +1;
 char str[9]; 
-if (n < 0)
+if (n > 98)
 plus = -1;
 while(n != 98)
 {
@@ -35,7 +35,7 @@ _putchar('0' + abs(n));
 }
 _putchar(',');
 _putchar(' ');
-n=n+plus;
+n = n + plus;
 }
 _putchar('9');
 _putchar('8');
