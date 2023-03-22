58b2c8a93a7a440fa112f7442e94a5b3846bce39
farida-O <faridaomar150@gmail.com>
2023-03-22 02:52:27 +0200

    9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * times_table - prints times table
+ * Description: 
+ * Return: void
+ */
+void times_table(void)
+{
+int i4 = 0;
+int i3 = 0;
+int n;
+for (; i4 <= 0; i4++)
+{
+for (; i3 <= 9; i3++)
+{
+n = i3 * i4;
+_putchar('0'+ n);
+}
+}
+}
