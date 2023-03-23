aa116cda0f61e58367055c25bfb66e4b2bf576c9
farida-O <faridaomar150@gmail.com>
2023-03-24 00:34:18 +0200

    1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include <ctype.h>
+#include "main.h"
+/**
+ * _isalpha - Determine whether it is an ahphabetic character
+ * Description: using islower
+ * @c: input Character
+ * Return: 0 in case it's an ahphabetic character and 1 otherwise
+ */
+int _isdigit(int c)
+{
+if (isdigit(c))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
