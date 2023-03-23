7dd6e386321123252e233136a1d0cc1808a3e45e
farida-O <faridaomar150@gmail.com>
2023-03-24 00:45:19 +0200

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - function
+ * Description: print alphabets
+ * return: void
+ */
+void print_numbers(void)
+{
+char c = '0';
+while (c <= '9')
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
+}
