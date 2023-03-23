52a24516e2bb387517efb065bc5f2cecc8f14c66
farida-O <faridaomar150@gmail.com>
2023-03-24 01:39:08 +0200

    6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - function
+ * Description: print numbers
+ * @n: number
+ * return: void
+ */
+void print_line(int n)
+{
+int j = 0;
+for (; j < n; j++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
