d0fa736121b5d0a16f8ea6370db1a92fb74686b9
farida-O <faridaomar150@gmail.com>
2023-03-24 02:57:44 +0200

    9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,6 +1,5 @@
 #include "main.h"
-#include <unistd.h>
-#include <string.h>
+#include <stdio.h>
 /**
  * main - causes an infinite loop
  * Return: 0
@@ -8,39 +7,23 @@
 int main(void)
 {
 int i = 1;
-int num;
-int l;
-int j;
-char str[10];
-char Fizz[] = "Fizz";
-char Buzz[] = "Buzz";
 for (; i <= 100; i++)
 {
 if (i%3 == 0)
 {
-write(STDOUT_FILENO, Fizz, 4);
+printf("Fizz");
 if (i%5 == 0)
 {
-write(STDOUT_FILENO, Buzz, 4);
+printf("Buzz");
 }
 }
 else if (i%5 == 0)
 {
-write(STDOUT_FILENO, Buzz, 4);
+printf("Buzz");
 }
 else
 {
-j = 0;
-num = i;
-while (num > 0) {
-str[j++] = num % 10 + '0';
-num /= 10;
-}
-l = j - 1;
-for (; l >= 0; l--)
-{
-write(STDOUT_FILENO, &str[l], 1);
-}
+pritf("%d", i);
 }
 _putchar(' ');
 }
