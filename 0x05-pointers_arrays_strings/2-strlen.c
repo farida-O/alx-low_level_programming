f1bb279796a34746cae13e92baa279f731db2079
farida-O <faridaomar150@gmail.com>
2023-03-28 00:29:38 +0200

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to a string
+ * Return: the length of a string
+ */
+int _strlen(char *s)
+{
+return (strlen(*s));
+}
