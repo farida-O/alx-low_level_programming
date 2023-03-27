fda27995b386b9acc9ad32362ec956ede303a070
farida-O <faridaomar150@gmail.com>
2023-03-28 01:27:12 +0200

    3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts -  prints a string, followed by a new line
+ * @str: pointer to a string
+ * Return: void
+ */
+void _puts(char *str)
+{
+puts(*str);
+}
