1de441970d0e7313349a746b60384c0c065b62d8
farida-O <faridaomar150@gmail.com>
2023-03-28 01:31:43 +0200

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_rev -  prints a string in a reverse, followed by a new line
+ * @s: pointer to a string
+ * Return: void
+ */
+void print_rev(char *s)
+{
+int i = strlen(s)-1;
+for (; i >= 0; i--)
+{
+puts(s[i]);
+}
+}
