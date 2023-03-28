b3799e87a8a444d8e265c570444c83d72cf5d70f
farida-O <faridaomar150@gmail.com>
2023-03-28 03:51:57 +0200

    8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * puts_half - prints n elements of an array of integers
+ * print_array - prints n elements of an array of integers
  * @a: pointer to an array
  * @n: number of elements of the array to be printed
  * Return: void
