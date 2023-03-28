e2ff7267aa5061dd165d63acf7f6b1e311597218
farida-O <faridaomar150@gmail.com>
2023-03-28 04:55:27 +0200

    9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strcpy -  copies the string pointed
+ * @dest: pointer to a string
+ * @src:  pointer to a string
+ * Return: char
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i = 0;
+for (; i < strlen(src); i++)
+{
+}
+return (dest);
+}
