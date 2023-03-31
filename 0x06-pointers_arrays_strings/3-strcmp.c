2a27173a33a348f2e6a6858eee67b9b84a94aeac
farida-O <faridaomar150@gmail.com>
2023-03-31 04:10:47 +0200

    3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,11 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _strcmp - concatenates two strings
+ * @s1: pointer to a string
+ * Return: int
+ */
+int _strcmp(char *s1, char *s2)
+{
+return (strcmp(s1, s2));
+}
