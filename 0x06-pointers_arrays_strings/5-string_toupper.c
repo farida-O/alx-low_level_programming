1a1ff9eac14153c31348dfc8eb2ed6ad60df3345
farida-O <faridaomar150@gmail.com>
2023-03-31 04:54:36 +0200

    5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,17 +1,16 @@
 #include <string.h>
-#include "main.h"
 /**
  * string_toupper - concatenates two strings
  * @s: pointer to a string
  * Return: string
  */
-char *string_toupper(char * s)
+char *string_toupper(char *s)
 {
 int i;
 i = 0;
 for (; i < strlen(s); i++)
 {
-if (s[i] > 96)
+if (s[i] > 'a')
 {
 s[i] -= 12;
 }
