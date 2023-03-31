69467524846a200ff7bebf70ef3b220b6c2574e0
farida-O <faridaomar150@gmail.com>
2023-03-31 04:20:48 +0200

    4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * reverse_array - concatenates two strings
+ * reverse_array - reverse array
  * @a: pointer to a string
  * @n:  number of elements of the array
  * Return: void
@@ -9,7 +9,15 @@ void reverse_array(int *a, int n)
 {
 int i = 0;
 int tmp;
-for (; i < n / 2; i++)
+if (n % 2 == 0)
+{
+n = n / 2;
+}
+else
+{
+n = (n + 1) / 2;
+}
+for (; i < n; i++)
 {
 tmp = a[i];
 a[i] = a[n - i];
