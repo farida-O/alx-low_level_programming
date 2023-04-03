ab689d35034151aea35277f07278f543b69b2423
farida-O <faridaomar150@gmail.com>
2023-04-03 06:39:47 +0200

    0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include <string.h>
+/**
+ * cap_string - concatenates two strings
+ * @s: pointer to a string
+ * Return: string
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+int i;
+i = 0;
+for (; i < n; i++)
+{
+*(s + i) = b;
+}
+return (s);
+}
