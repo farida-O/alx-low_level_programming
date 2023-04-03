d5f375e29b850bdaffb07d4ec45d8e62f3ea892f
farida-O <faridaomar150@gmail.com>
2023-04-03 08:20:06 +0200

    2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,11 @@
+#include <string.h>
+/**
+ * _strchr -  locates a character in a string
+ * @s: pointer to a string
+ * @c: charachter
+ * Return: pointer to the charachter
+ */
+char *_strchr(char *s, char c)
+{
+return (strchr(s, c));
+}
