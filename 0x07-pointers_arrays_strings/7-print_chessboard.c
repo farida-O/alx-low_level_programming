4f826b1732e3f0966f883dbdcc8ae563fa6fae94
farida-O <faridaomar150@gmail.com>
2023-04-03 09:20:35 +0200

    7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include <stdio.h>
+/**
+ * print_chessboard -  prints the chessboard
+ * @a: pointer to a string
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+int i, j;
+i = 0;
+for (; i < 8; i++)
+{
+j = 0;
+for (; j < 8; j++)
+{
+_putchar(a[i][j]);
+}
+_putchar('\n');
+}
+}
