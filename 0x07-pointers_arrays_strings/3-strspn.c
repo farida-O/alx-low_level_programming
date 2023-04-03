46ea8e5d248399273c1f2e7f8bf2e0bc7d280c39
farida-O <faridaomar150@gmail.com>
2023-04-03 08:29:05 +0200

    3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -3,7 +3,7 @@
  * _strspn -  gets the length of a prefix substring.
  * @s: pointer to a string
  * @accept: charachter
- * Return: pointer to the charachter
+ * Return: number
  */
 unsigned int _strspn(char *s, char *accept)
 {
