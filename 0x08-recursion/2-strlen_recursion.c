3ce730cf8607e5afb967aba29fea9f4b034d27be
farida-O <faridaomar150@gmail.com>
2023-04-05 17:22:03 +0200

    1

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: pointer to a string
+ * Return: integer
+ */
+int _strlen_recursion(char *s)
+{
+return (strlen(s));
+}
