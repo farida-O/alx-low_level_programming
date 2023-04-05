5f7bd400df62080cb8e1319d6c338957ff8185a7
farida-O <faridaomar150@gmail.com>
2023-04-05 17:32:34 +0200

    3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-#include <string.h>
 /**
- * _strlen_recursion - returns the length of a string
- * @s: pointer to a string
+ * factorial - returns the factorial
+ * @n: number
  * Return: integer
  */
 int factorial(int n)
