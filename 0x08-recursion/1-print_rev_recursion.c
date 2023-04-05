8d4be33ea0b89706c1520c884b47ba84faab803d
farida-O <faridaomar150@gmail.com>
2023-04-05 17:10:03 +0200

    1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * _print_rev_recursion -  prints a string
+ * @s: pointer to a string
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+int i = strlen(s) - 1;
+for (; i >= 0; i--)
+{
+putchar(s[i]);
+}
+putchar('\n');
+}
