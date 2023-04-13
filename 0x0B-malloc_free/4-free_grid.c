b70f8441777f06cbe0d12045223298fcd2d95263
farida-O <faridaomar150@gmail.com>
2023-04-13 13:22:03 +0200

    4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+/**
+ * free_grid -  concatenates two strings
+ * @grid: pointer to pointer
+ * @height: string
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+int i = 0;
+for (; i < height; i++)
+{
+free(grid[i]);
+free(grid);
+return (NULL);
+}
+}
