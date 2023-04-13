8a2a64eca9bcdb8b0aadd71c15c8e6b2c7dc57ed
farida-O <faridaomar150@gmail.com>
2023-04-13 16:06:48 +0200

    0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <string.h>
+/**
+ * malloc_checked - creates a copy of the string
+ * @b: string
+ * Return: void
+ */
+void *malloc_checked(unsigned int b)
+{
+int *arr = NULL;
+arr = malloc(sizeof(b));
+arr = b;
+return (arr);
+}
