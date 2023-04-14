522b0395915e414564cd2e419e5c2da984cf2c17
farida-O <faridaomar150@gmail.com>
2023-04-14 17:06:55 +0200

    3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -3,16 +3,15 @@
  * array_range - allocates memory using malloc
  * @min: string
  * @max: string
- * Return: integer
+ * Return: pointer
  */
 int *array_range(int min, int max)
 {
 int *arr;
 int i = 0;
-int size = max - min;
 if (min > max)
 return (NULL);
-arr = malloc(size + 1);
+arr = malloc(max - min + 1);
 if (arr == NULL)
 return (NULL);
 for (; min <= max; min++)
