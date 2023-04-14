53559baa96aadd56492f289326c52f9b128b0592
farida-O <faridaomar150@gmail.com>
2023-04-14 11:24:15 +0200

    1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+/**
+ * string_nconcat - allocates memory using malloc
+ * @s1: string
+ * @s2:
+ * @n:
+ * Return: void
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+char *arr = NULL;
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
+arr = malloc(strlen(s1) + strlen(s2) + 1);
+if (arr == NULL)
+{
+return (NULL);
+}
+if (s1)
+strcpy(arr, s1);
+if (s2)
+strncat(arr, s2, n);
+return (arr);
+}
