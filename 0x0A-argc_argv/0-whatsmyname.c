64c073f4fdbd023bc0a4c06e494872f8dd544699
farida-O <faridaomar150@gmail.com>
2023-04-15 13:06:11 +0200

    0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,8 +5,8 @@
  * @argv: arguments vectors
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__ ((unused)), char *argv[])
 {
-printf("%s", argv[0]);
+printf("%s\n", argv[0]);
 return (0);
 }
