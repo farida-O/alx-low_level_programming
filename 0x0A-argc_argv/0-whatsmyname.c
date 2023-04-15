d18727886c2de8fd1b8a0a5e006164e543a14bd1
farida-O <faridaomar150@gmail.com>
2023-04-15 12:55:39 +0200

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Starting point
+ * @argc: arguments counts
+ * @argv: arguments vectors
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+printf("%s", argv[0]);
+return (0);
+}
