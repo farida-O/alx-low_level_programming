ceef52bb5c65e683ac7eac61244a4f2f738defc3
farida-O <faridaomar150@gmail.com>
2023-04-15 13:19:43 +0200

    2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - Starting point
- * @argc: arguments counts
- * @argv: arguments vectors
+ * main - starting point.
+ * @argc: arguments counts.
+ * @argv: arguments vectors.
  * Return: 0
  */
 int main(int argc, char *argv[])
