1c7a80397a9a7b8325d85ffcd9e925c7c3cffb2b
farida-O <faridaomar150@gmail.com>
2023-04-18 08:12:06 +0200

    2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdio.h>
 /**
  * init_dog - initialize a variable of type struct dog.
  * @d: a pointer to struct.
@@ -6,7 +7,7 @@
  */
 void print_dog(struct dog *d)
 {
-!(d-> name) ? printf("Name: %s", d-> name) : printf("Name: (nil)");
-!(d-> age) ? printf("Age: %f", d-> age) : printf("Name: (nil)");
-!(d-> owner) ? printf("Owner: %s", d-> owner) : printf("Name: (nil)");
+(d-> name) ? printf("Name: %s\n", d-> name) : printf("Name: (nil)");
+(d-> age) ? printf("Age: %f\n", d-> age) : printf("Age: (nil)");
+(d-> owner) ? printf("Owner: %s\n", d-> owner) : printf("Owner: (nil)");
 }
