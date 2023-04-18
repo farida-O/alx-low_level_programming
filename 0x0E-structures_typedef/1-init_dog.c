452800816133c4f158dc692efcc11f6d04b122d7
farida-O <faridaomar150@gmail.com>
2023-04-18 07:13:59 +0200

    c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+/**
+ * str_concat -  concatenates two strings
+ * @s1: string
+ * @s2: string
+ * Return:  a pointer to the array or NULL if size = 0
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d-> name = name;
+d-> age = age;
+d-> owner = owner;
+}
