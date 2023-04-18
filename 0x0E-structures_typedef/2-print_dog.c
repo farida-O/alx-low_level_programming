f3b7619f6cad1755a7e5aa1b2d3fee3398e5cd65
farida-O <faridaomar150@gmail.com>
2023-04-18 08:23:52 +0200

    2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,7 +11,7 @@ if (d == NULL)
 {
 return;
 }
-(d->name != NULL) ? printf("Name: %s\n", d-> name) : printf("Name: (nil)\n");
-(d->age != 0) ? printf("Age: %f\n", d-> age) : printf("Age: (nil)\n");
-(d->owner != NULL) ? printf("Owner: %s\n", d-> owner) : printf("Owner: (nil)\n");
+(d->name != NULL) ? printf("Name: %s\n", d->name) : printf("Name: (nil)\n");
+(d->age != 0) ? printf("Age: %f\n", d->age) : printf("Age: (nil)\n");
+(d->owner != NULL) ? printf("Owner: %s\n", d->owner) : printf("Owner: (nil)\n");
 }
