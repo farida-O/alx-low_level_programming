03d4ab0a35d91ebaeacd7920d589049f9e7db5b4
farida-O <faridaomar150@gmail.com>
2023-04-18 22:02:14 +0200

    c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,4 +1,6 @@
 #include "dog.h"
+#include <string.h>
+#include <stdlib.h>
 /**
  * new_dog - initialize a variable of type struct dog.
  * @name: dog name.
@@ -8,11 +10,23 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *d;
-dog_t d2;
-d = &d2;
-d-> name = name;
-d-> age = age;
-d-> owner = owner;
+dog_t *d = malloc(sizeof(dog_t));
+if (d == NULL)
+{
+return (NULL);
+}
+d->name = malloc(sizeof(name) + 1);
+if (d->name == NULL)
+{
+return (NULL);
+}
+strcpy(d->name, name);
+d->age = age;
+d->owner = malloc(sizeof(owner) + 1);
+if (d->owner == NULL)
+{
+return (NULL);
+}
+strcpy(d-> owner, owner);
 return (d);
 }
