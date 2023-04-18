d343d4ef447dd83c30d42a80bb0da28cf0170a98
farida-O <faridaomar150@gmail.com>
2023-04-18 22:05:31 +0200

    c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,11 +13,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 dog_t *d = malloc(sizeof(dog_t));
 if (d == NULL)
 {
+free(d);
 return (NULL);
 }
 d->name = malloc(sizeof(name) + 1);
 if (d->name == NULL)
 {
+free(d->name);
+free(d);
 return (NULL);
 }
 strcpy(d->name, name);
@@ -25,6 +28,8 @@ d->age = age;
 d->owner = malloc(sizeof(owner) + 1);
 if (d->owner == NULL)
 {
+free(d->owner);
+free(d);
 return (NULL);
 }
 strcpy(d-> owner, owner);
