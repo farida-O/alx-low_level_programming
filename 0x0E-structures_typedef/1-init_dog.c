262c3e032530efbd7bd7bba11977d0a16f66cdc1
farida-O <faridaomar150@gmail.com>
2023-04-18 07:19:47 +0200

    c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,9 +1,11 @@
 #include "dog.h"
 /**
- * str_concat -  concatenates two strings
- * @s1: string
- * @s2: string
- * Return:  a pointer to the array or NULL if size = 0
+ * init_dog - initialize a variable of type struct dog.
+ * @d: a pointer to struct.
+ * @name: dog name.
+ * @owner: owner.
+ * @age: age.
+ * Return: void
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
