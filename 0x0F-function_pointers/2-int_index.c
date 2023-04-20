1f6a0df75d01baa9752bba08643bcbc4bb516e1c
farida-O <faridaomar150@gmail.com>
2023-04-20 03:41:26 +0200

    c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * int_index - executes a function given as a parameter.
+ * @array: array of elements.
+ * @size: the size of the array.
+ * @cmp: pointer to the function you need to use.
+ * Return:  the index of the first element.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int flag = 0;
+int i = 0;
+if (cmp != NULL)
+{
+for (; i < size; i++)
+{
+flag = cmp(array[i]);
+if (flag)
+{
+return (i);
+}
+}
+return (-1);
+}
+}
+
