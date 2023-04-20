f232d8f2b679228793a4b8b900bf1e34f8500afd
farida-O <faridaomar150@gmail.com>
2023-04-20 05:58:07 +0200

    c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,13 +1,11 @@
-#include "calc.h"
+#include "3-calc.h"
 #include <stdlib.h>
 /**
  * get_op_func - selects the correct function to perform the operation.
  * @s: the operator
- * @a:
- * @b:
  * Return: integr.
  */
-int (*get_op_func(char *s))(int a, int b)
+int (*get_op_func(char *s))(int, int)
 {
 op_t ops[] = {
 {"+", op_add},
@@ -18,7 +16,15 @@ op_t ops[] = {
 {NULL, NULL}
 };
 int i;
-ops[]->op;
-ops[]->f(a, b);
+i = 0;
+while (ops[i].op != NULL)
+{
+if (ops[i].op == s)
+{
+return (ops[i].f);
+}
+i++;
+}
+return (NULL);
 }
 
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "3-calc.h"
 /**
  * main - starting point
  * @argc: arguments counts
@@ -10,11 +11,27 @@
 int main(int argc, char *argv[])
 {
 int a, b;
+char *s;
+int (*func_ptr)(int, int);
 if (argc != 4)
 {
 printf("Error/n");
+exit(98);
 }
 a = atoi(argv[1]);
 b = atoi(argv[3]);
+s = argv[2];
+if ((strcmp(s, "%") || strcmp(s, "/")) && !b)
+{
+printf("Error/n");
+exit(100);
+}
+func_ptr = get_op_func(s);
+if (func_ptr == NULL)
+{
+printf("Error/n");
+exit(99);
+}
+printf("%d\n", (*func_ptr)(a, b));
 return (0);
 }
