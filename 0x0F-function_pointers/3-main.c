91c3d79141a6957dcb6ee4e881cc44e2d00dec1c
farida-O <faridaomar150@gmail.com>
2023-04-20 05:00:03 +0200

    c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * main - starting point
+ * @argc: arguments counts
+ * @argv: arguments vectors
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+int a, b;
+if (argc != 4)
+{
+printf("Error/n");
+}
+a = atoi(argv[1]);
+b = atoi(argv[3]);
+return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/**
+ * op_add - summation operation.
+ * @a: array of elements.
+ * @b: the size of the array.
+ * Return: sum of type integer.
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+/**
+ * op_sub - Difference opertaion.
+ * @a: array of elements.
+ * @b: the size of the array.
+ * Return: the difference of type integer.
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+ * op_mul - multiplication operation.
+ * @a: array of elements.
+ * @b: the size of the array.
+ * Return: the difference of type integer.
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+/**
+ * op_div - division operation.
+ * @a: array of elements.
+ * @b: the size of the array.
+ * Return: the difference of type integer.
+ */
+int op_div(int a, int b)
+{
+return (a / b);
+}
+/**
+ * op_mod - remainder of the devision operation.
+ * @a: array of elements.
+ * @b: the size of the array.
+ * Return: the difference of type integer.
+ */
+int op_mod(int a, int b)
+{
+return (a % b);
+}
