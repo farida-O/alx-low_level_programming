136100a811814dd0ade4331643ea3fed93a171c7
farida-O <faridaomar150@gmail.com>
2023-04-20 04:05:17 +0200

    c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,7 +9,7 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 int i = 0;
-if (action != NULL)
+if ((action != NULL) && (size > 0) && (array != NULL))
 {
 for (; i < (int)size; i++)
 {
