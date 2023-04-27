ecb6ae09e67c729d90782c9caf4b5da5049d447a
farida-O <faridaomar150@gmail.com>
2023-04-27 23:35:52 +0200

    c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * list_len - prints anything.
+ * @h: list head.
+ * Return:  the number of nodes.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+list_t current;
+current.str = strdup(str);
+current.next = *head;
+return (&current);
+}
