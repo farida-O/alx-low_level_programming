86861ebbea7bada7e82b4953bcff53d19a305ba7
farida-O <faridaomar150@gmail.com>
2023-04-27 23:51:52 +0200

    c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,7 +11,15 @@ list_t *add_node(list_t **head, const char *str)
 {
 list_t *current;
 current = malloc(sizeof(list_t));
+if (current == NULL)
+{
+return (NULL);
+}
 current->str = malloc(strlen(str) + 1);
+if (current->str == NULL)
+{
+return (NULL);
+}
 current->str = strdup(str);
 current->next = *head;
 return (current);
