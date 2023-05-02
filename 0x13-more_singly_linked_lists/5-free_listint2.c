f3f2fda6153beee7bf0d2e3af36a36dfcd52b552
farida-O <faridaomar150@gmail.com>
2023-05-02 18:11:41 +0200

    c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint - prints anything.
+ * @head: list head.
+ * Return: void.
+ */
+void free_listint2(listint_t **head)
+{
+listint_t *current = *head;
+listint_t *prev = current;
+while (current != NULL)
+{
+prev = current;
+current = current->next;
+free(prev);
+}
+*head = NULL;
+}
