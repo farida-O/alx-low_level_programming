d65a886f8adafde4daa8b8ddd4ce32f30df2b726
farida-O <faridaomar150@gmail.com>
2023-05-02 18:37:11 +0200

    c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- * free_listint2 - prints anything.
+ * free_listint2 - frees.
  * @head: list head.
  * Return: void.
  */
@@ -10,12 +10,11 @@ void free_listint2(listint_t **head)
 {
 listint_t *current = *head;
 listint_t *prev = current;
+*head = NULL;
 while (current != NULL)
 {
 prev = current;
 current = current->next;
 free(prev);
 }
-*head = NULL;
-head = NULL;
 }
